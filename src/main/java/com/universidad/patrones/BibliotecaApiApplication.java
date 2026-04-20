e0d56ff8f4d31cd7b8be93f9ebac7b44a48ebcc1
Abrahan Remolina Bolivar <abrahanremolina703@gmail.com>
2026-04-19 22:26:43 -0500

    feat: implementar DTOs, mapper, manejo global de errores y controlador v2 con Swagger

--- src/main/java/com/universidad/patrones/BibliotecaApiApplication.java
+++ src/main/java/com/universidad/patrones/BibliotecaApiApplication.java
@@ -1,4 +1,4 @@
-package com.universidad.patrones.biblioteca_api;
+package com.universidad.patrones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class BibliotecaApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApiApplication.class, args);
 	}
-
 }
